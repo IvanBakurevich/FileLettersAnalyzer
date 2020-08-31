781c91811d30af3198d896cfd60dc8e2a2d14e29
IvanBakurevich <bakurevich.ivan@gmail.com>
2020-08-31 20:16:17 +0300

    Add documentation comments.

--- CharacterCounter.cpp
+++ CharacterCounter.cpp
@@ -1,26 +1,48 @@
 #include<string>
 
+/**
+ * Class represents a container for character
+ * and its count.
+ */
 class CharacterCounter {
 private:
     char charValue;
     int count;
 
 public:
+    /**
+     * Constructs a new object instance for character
+     * with its one copy.
+     */
     CharacterCounter(char charValue) {
         this->charValue = charValue;
         this->count = 1;
     }
 
+    /**
+     * Returns character value.
+     */
     char getCharValue() {
         return charValue;
     }
+    /**
+     * Returns character's count.
+     */
     int getCount() {
         return count;
     }
 
+    /**
+     * Adds one copy of character
+     * from the container.
+     */
     void addCharInstance() {
         count++;
     }
+
+    /**
+     * Returns a string representation of the object.
+     */
     std::string toString() {
         return std::string(1, charValue) + " - " + std::to_string(count);
     }
--- LettersAnalyzer.cpp
+++ LettersAnalyzer.cpp
@@ -5,11 +5,19 @@
 
 using namespace std;
 
+/**
+ * Class represents a container that 
+ * contains set of letters with
+ * each letter's count. 
+ */
 class LettersAnalyzer {
 private:
     vector<CharacterCounter> letters;
 
 public:
+    /**
+     *Shows whether character is letter from English or Russian alphabet.
+     */
     static bool isRusEngLetter(char ch) {
         if (isalpha(ch) || (ch >= 'À' && ch <= 'ÿ')) {
             return true;
@@ -17,11 +25,21 @@ public:
         return false;
     }
 
+    /**
+     * Adds every letter from string to the container.
+     */
     void addFromString(string str) {
         for (int i = 0; i < str.length(); i++) {
             addLetter(str[i]);
         }
     }
+
+    /**
+     * Adds character instance to the container,
+     * if character is a letter from English or Russian alphabet
+     * and return true, otherwise return false.
+     * All letters converts to lowercase.
+     */
     bool addLetter(char ch) {
         if (!isRusEngLetter(ch)) {
             return false;
@@ -37,11 +55,19 @@ public:
         return true;
     }
 
+    /**
+     * Sort letters from the container based on
+     * their count in descending order.
+     */
     void sortByÂescendingCount() {
         sort(letters.begin(), letters.end(),
             [](CharacterCounter& a, CharacterCounter& b) { return a.getCount() > b.getCount(); }
         );
     }
+
+    /**
+    * Returns a string representation of the object.
+    */
     string toString() {
         string result;
         for (auto& item : letters) {
