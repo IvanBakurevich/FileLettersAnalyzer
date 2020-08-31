5c8861d78abed87d8e76ed78981b467018b4bcc6
IvanBakurevich <bakurevich.ivan@gmail.com>
2020-08-31 22:18:28 +0300

    Add exception handling.

--- FileLettersAnalyzer.cpp
+++ FileLettersAnalyzer.cpp
@@ -6,33 +6,43 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    if (argc != 3) {
-        cout << "Exact 2 arguments required: INPUT_FILE OUTPUT_FILE" << endl;
-        return 0;
-    }
-
-    ifstream in(argv[1]);
-    ofstream out(argv[2]);
-
-    if (!in.is_open()) {
-        cout << "Invalid INPUT_FILE: " << argv[1] << endl;
-    }
-    else {
-        setlocale(LC_ALL, "ru");
-        LettersAnalyzer letterAnalyzer;
-        string line;
-
-        while (getline(in, line)) {
-            letterAnalyzer.addFromString(line);
+    try {
+        if (argc != 3) {
+            throw 0;
         }
 
-        letterAnalyzer.sortByDescendingCount();
-        if (out.is_open()) {
-            out << letterAnalyzer.toString();
-            cout << "Success program execution!" << endl;
+        ifstream in(argv[1]);
+        ofstream out(argv[2]);
+
+        if (!in.is_open()) {
+            throw 1;
+        }
+        else {
+            setlocale(LC_ALL, "ru");
+            LettersAnalyzer letterAnalyzer;
+            string line;
+
+            while (getline(in, line)) {
+                letterAnalyzer.addFromString(line);
+            }
+
+            letterAnalyzer.sortByDescendingCount();
+            if (out.is_open()) {
+                out << letterAnalyzer.toString();
+                cout << "Success program execution!" << endl;
+            }
+        }
+
+        in.close();
+        out.close();
+    }
+    catch (int e) {
+        cerr << "Exception: ";
+        if (e == 0) {
+            cerr << "Exact 2 arguments required: INPUT_FILE OUTPUT_FILE" << endl;
+        }
+        if (e == 1) {
+            cerr << "Invalid INPUT_FILE path - " << argv[1] << endl;
         }
     }
-
-    in.close();
-    out.close();
 }
